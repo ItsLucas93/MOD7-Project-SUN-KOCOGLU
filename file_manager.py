e1903eea847a825317f07e8c30fcfea1f5d45cb6
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-12-06 23:06:34 -0500

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -5,7 +5,12 @@ def load_file(file_path):
     except FileNotFoundError:
         raise FileNotFoundError(f'File {file_path} not found')
 
+    # Verify that the file is a valid file: can be divided into 32-bit chunks
     if len(content) % 32 != 0:
         raise ValueError(f'File {file_path} is not a valid file (Bits counting is not a multiple of 32: len = {len(content), len(content) % 32})')
+
+    # Verify that the file is a valid file: only 0 and 1
+    if not all(c in '01' for c in content):
+        raise ValueError(f'File {file_path} is not a valid file (Not only 0 and 1)')
 
     return content
